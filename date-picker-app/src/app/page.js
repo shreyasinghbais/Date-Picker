49567bbcaa5969ae3c07526ff948704fa50dd805
Shreya Singh Bais <124716473+shreyasinghbais@users.noreply.github.com>
2024-09-28 09:40:00 +0000

    commit

--- date-picker-app/src/app/page.js
+++ date-picker-app/src/app/page.js
@@ -1,4 +1,4 @@
- import { DatePicker } from '../components/DatePicker';
+import { DatePicker } from '../components/DatePicker';
 import { RecurrenceSettingsProvider } from '../components/RecurrenceContext';
 
 export default function Home() {
